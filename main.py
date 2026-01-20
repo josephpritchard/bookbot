2e0aa79c7f1fc182d93b514108918e2aa3466024
etzi-dev <247473336+etzi-dev@users.noreply.github.com>
2026-01-20 00:06:32 -0500

    counter print working, print select longest words working. filtering out symbols and blanks still needs to be done, plus additional tweaking.

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ def print_report(report_dict):
             count = d["num"] 
             print(f"{character}: {count}")
 
+def print_report_integers(report_dict):
+    for d in report_dict:
+        character = d["char"]
+        count = d["num"] 
+        print(f"{character}: {count}")
+
 def startup():
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
@@ -24,15 +30,21 @@ def main():
     text = get_book_text(sys.argv[1])
     num_words = get_word_num(text) 
     char_dict = get_char_stats(text)
-    sorted_dict = sort_dict(char_dict)
+    sorted_dict = sort_dict(char_dict, False)
+    lengths_dict = get_word_lengths(text)
+    sorted_lengths_dict = sort_dict(lengths_dict, True)
+    longest_dict = get_longest_words(text)
+    sorted_longest_dict = sort_dict(longest_dict, True)
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {sys.argv[1]}") 
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------") 
     print_report(sorted_dict)
+    print("--------Word Length Count -------") 
+    print_report_integers(sorted_lengths_dict)
+    print("---------Longest Words-----------")
+    print_report_integers(sorted_longest_dict)
     print("============= END ===============")
 
-    d7 = get_word_len(text)
-    print(f"d7 is {d7}")
 main()
--- stats.py
+++ stats.py
@@ -14,13 +14,13 @@ def get_char_stats(file_contents):
     return d
 
 def sort_key(item):
-    return item["num"]
+    return item["char"]
 
-def sort_dict(new_d):
+def sort_dict(new_d, rev_bool):
     split_d = []
     for k, v in new_d.items():
         split_d.append(dict([("char", k), ("num", v)]))
-    split_d.sort(reverse=True, key=sort_key)
+    split_d.sort(reverse=rev_bool, key=sort_key)
     return split_d
 
 def get_word_len(file_contents):
@@ -30,9 +30,35 @@ def get_word_len(file_contents):
           {"Number": 0,
            "Words": []}
           }
+    d7_words = d7["Seven Letter Words"]["Words"]
     words = file_contents.split()
     for word in words:
-        if (len(word) == 7) & (len(d7["Seven Letter Words"]["Words"]) < 10):
-            d7["Seven Letter Words"]["Number"] += 1
-            d7["Seven Letter Words"]["Words"].append(word) 
+        if (len(word) == 7):
+            d7_words.append(word) 
+    d7["Seven Letter Words"]["Words"] = set(d7_words)
+    d7["Seven Letter Words"]["Number"] = len(d7_words)
     return d7
+
+def get_word_lengths(file_contents):
+    words = file_contents.split()
+    d_lengths = {}
+    for word in words:
+        if len(word) not in d_lengths:
+            d_lengths[len(word)] = 1
+        else:
+            d_lengths[len(word)] += 1
+    return d_lengths
+
+def get_longest_words(file_contents):
+    d_longest = {}
+    words = file_contents.split()
+    for i in range(17, 33):
+        d_longest[i] = []
+        for word in words:
+            if len(word) == i:
+                d_longest[i].append(word)
+    return d_longest
+#    for i in range(2):
+#        character = d["char"]
+#        count = d["num"]
+#        print(f"{character}: {count}")
